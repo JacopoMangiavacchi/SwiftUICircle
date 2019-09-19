e0603ee7a542e711a480db2bad23c56f3bab3205
Jacopo Mangiavacchi <jmangia@me.com>
2019-09-19 17:25:48 -0500

    generalize row col data

--- iOS/Circles/ContentView.swift
+++ iOS/Circles/ContentView.swift
@@ -8,9 +8,9 @@
 
 import SwiftUI
 
-let colors = [UIColor.white, UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.cyan, UIColor.purple]
+let colors = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.cyan, UIColor.purple, UIColor.brown]
 
-struct CircleData {
+struct CircleRowColData {
     let speed: Int
     let color: UIColor
 }
@@ -30,55 +30,60 @@ struct ContentView: View {
     @State var animationTime = 10.0
     @State var modalDisplayed = false
     @State var startAngle = 90
-    @State var rows:[CircleData] = {
-        var array = [CircleData]()
-        for i in 1...7 {
-            array.append(CircleData(speed: i, color: colors[i]))
+    @State var rows:[CircleRowColData] = {
+        var array = [CircleRowColData]()
+        for i in 0..<8 {
+            array.append(CircleRowColData(speed: i + 1, color: colors[i % colors.count]))
         }
         return array
     }()
-    @State var columns:[CircleData] = {
-        var array = [CircleData]()
-        for i in 1...7 {
-            array.append(CircleData(speed: i, color: colors[i]))
+    @State var columns:[CircleRowColData] = {
+        var array = [CircleRowColData]()
+        for i in 0..<8 {
+            array.append(CircleRowColData(speed: i + 1, color: colors[i % colors.count]))
         }
         return array
     }()
-
-    func color(r: Int, c: Int) -> Color {
-        if c == 0 {
-            return Color(colors[r])
-        }
-
-        if r == 0 {
-            return Color(colors[c])
-        }
-        
-        let comp1 = colors[r].cgColor.components!
-        let comp2 = colors[c].cgColor.components!
-
-        return Color(red: Double(comp1[0] + comp2[0]) / 2.0,
-                     green: Double(comp1[1] + comp2[1]) / 2.0,
-                     blue: Double(comp1[2] + comp2[2]) / 2.0)
-    }
 
     var body: some View {
         HStack {
-            ForEach((0..<8), id: \.self) { r in
+            // Columns
+            ForEach((0...self.columns.count), id: \.self) { c in
                 VStack {
-                    ForEach((0..<8), id: \.self) { c in
+                    // Rows
+                    ForEach((0...self.rows.count), id: \.self) { r in
                         Group {
                             if r == 0 && c == 0 {
-                                CircleView(modalDisplayed: self.$modalDisplayed, pct: self.pct, startAngle: self.startAngle, color: self.color(r:0, c:0), text: "10s", r: 1, c: 1)
+                                CircleView(modalDisplayed: self.$modalDisplayed,
+                                           pct: self.pct,
+                                           startAngle: self.startAngle,
+                                           text: "10s",
+                                           row: CircleRowColData(speed: 1, color: UIColor.white),
+                                           col: CircleRowColData(speed: 1, color: UIColor.white))
                             }
                             else if r == 0 {
-                                CircleView(modalDisplayed: self.$modalDisplayed, pct: self.pct, startAngle: self.startAngle, color: self.color(r:r, c:c), text: "\(c)x", r: r, c: c)
+                                CircleView(modalDisplayed: self.$modalDisplayed,
+                                           pct: self.pct,
+                                           startAngle: self.startAngle,
+                                           text: "\(c)x",
+                                           row: self.columns[c - 1],
+                                           col: self.columns[c - 1])
                             }
                             else if c == 0 {
-                                CircleView(modalDisplayed: self.$modalDisplayed, pct: self.pct, startAngle: self.startAngle, color: self.color(r:r, c:c), text: "\(r)x", r: r, c: c)
+                                CircleView(modalDisplayed: self.$modalDisplayed,
+                                           pct: self.pct,
+                                           startAngle: self.startAngle,
+                                           text: "\(r)x",
+                                           row: self.rows[r - 1],
+                                           col: self.rows[r - 1])
                             }
                             else {
-                                CircleView(modalDisplayed: self.$modalDisplayed, pct: self.pct, startAngle: self.startAngle, color: self.color(r:r, c:c), text: "", r: r, c: c)
+                                CircleView(modalDisplayed: self.$modalDisplayed,
+                                           pct: self.pct,
+                                           startAngle: self.startAngle,
+                                           text: "",
+                                           row: self.rows[r - 1],
+                                           col: self.columns[c - 1])
                             }
                         }
                     }
@@ -98,17 +103,38 @@ struct CircleView: View {
     @Binding var modalDisplayed: Bool
     let pct: Double
     let startAngle: Int
-    let color: Color
     let text: String
-    let r: Int
-    let c: Int
+    let row: CircleRowColData
+    let col: CircleRowColData
+    let color: Color
 
+    init(modalDisplayed: Binding<Bool>, pct: Double, startAngle: Int, text: String, row: CircleRowColData, col: CircleRowColData) {
+        self._modalDisplayed = modalDisplayed
+        self.pct = pct
+        self.startAngle = startAngle
+        self.text = text
+        self.row = row
+        self.col = col
+        
+        if row.color.cgColor.numberOfComponents < 3 || col.color.cgColor.numberOfComponents < 3 {
+            self.color = Color.white
+        }
+        else {
+            let comp1 = row.color.cgColor.components!
+            let comp2 = col.color.cgColor.components!
+
+            self.color = Color(red: Double(comp1[0] + comp2[0]) / 2.0,
+                         green: Double(comp1[1] + comp2[1]) / 2.0,
+                         blue: Double(comp1[2] + comp2[2]) / 2.0)
+        }
+    }
+    
     var body: some View {
         Button(action: { self.modalDisplayed = true }) {
-            CircleShape(pct: pct, startAngle: self.startAngle, r:r, c:c)
-                .stroke(color, lineWidth: 2.0)
+            CircleShape(pct: pct, startAngle: self.startAngle, xSpeed: col.speed, ySpeed: row.speed)
+                .stroke(self.color, lineWidth: 2.0)
                 .padding(2)
-                .overlay(Text(text).foregroundColor(color))
+                .overlay(Text(text).foregroundColor(self.color))
         }.sheet(isPresented: self.$modalDisplayed) {
             DetailView(onDismiss: {
                 self.modalDisplayed = false
@@ -122,15 +148,15 @@ struct CircleView: View {
 struct CircleShape: Shape {
     var pct: Double
     let startAngle: Int
-    let r: Int
-    let c: Int
+    let xSpeed: Int
+    let ySpeed: Int
     let x_cos: [Double]
     let y_sin: [Double]
     
-    init(pct: Double, startAngle: Int, r: Int, c: Int) {
+    init(pct: Double, startAngle: Int, xSpeed: Int, ySpeed: Int) {
         self.pct = pct
-        self.r = r
-        self.c = c
+        self.xSpeed = xSpeed
+        self.ySpeed = ySpeed
         self.startAngle = startAngle
         self.x_cos = (0..<360).map {1.0 + cos(Double(($0 - startAngle) % 360) * Double.pi/180)}
         self.y_sin = (0..<360).map {1.0 + sin(Double(($0 - startAngle) % 360) * Double.pi/180)}
@@ -138,36 +164,17 @@ struct CircleShape: Shape {
 
     func drawFigure(scaleX: Double, scaleY: Double) -> Path {
         return Path { p in
-            if r > 0 || c > 0 {
-                p.move(to: CGPoint(x: x_cos[359] * scaleX, y: y_sin[359] * scaleY))
+            p.move(to: CGPoint(x: x_cos[359] * scaleX, y: y_sin[359] * scaleY))
 
-                var (x, y) = (0.0, 0.0)
-                for i in 0..<Int(pct*360) {
-                    var ix = i
-                    var iy = i
-                    
-                    if r == 0 {
-                        ix *= c
-                        iy *= c
-
-                    }
-                    else if c == 0 {
-                        ix *= r
-                        iy *= r
-                    }
-                    else {
-                        ix *= r
-                        iy *= c
-                    }
-
-                    x = x_cos[ix % 360] * scaleX
-                    y = y_sin[iy % 360] * scaleY
-                    p.addLine(to: CGPoint(x: x, y: y))
-                }
-                
-                p.addEllipse(in: CGRect(x: x, y: y, width: 5, height: 5))
-                p.addEllipse(in: CGRect(x: x, y: y, width: 10, height: 10))
+            var (x, y) = (0.0, 0.0)
+            for i in 0..<Int(pct*360) {
+                x = x_cos[(i * xSpeed) % 360] * scaleX
+                y = y_sin[(i * ySpeed) % 360] * scaleY
+                p.addLine(to: CGPoint(x: x, y: y))
             }
+            
+            p.addEllipse(in: CGRect(x: x, y: y, width: 5, height: 5))
+            p.addEllipse(in: CGRect(x: x, y: y, width: 10, height: 10))
         }
     }
     
